d2d5cf0d18342ac50f036f9923a997b329b89e28
BrandonMFong <bfong@owc.com>
2021-11-25 04:51:15 +0000

    [SQUASH] patch/main/Brando-Week12

--- s3.tf
+++ s3.tf
@@ -16,7 +16,6 @@ resource "aws_s3_bucket" "ece592-athena-cache-v2" {
     Name = "ece592-athena-cache-v2"
   }
 
-
   policy = <<POLICY
 {
     "Version": "2012-10-17",
@@ -38,6 +37,62 @@ resource "aws_s3_bucket" "ece592-athena-cache-v2" {
             },
             "Action": "s3:PutObject",
             "Resource": "arn:aws:s3:::ece592-athena-cache-v2/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
+# Week 12
+resource "aws_flow_log" "ece592-flowlogs-log" {
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.week8-vpc-v2.id # The vpc is week8 because I screwed up on week8
+}
+
+resource "aws_s3_bucket" "ece592-flowlogs-brando" {
+  bucket        = "ece592-flowlogs-brando"
+  force_destroy = true
+  
+  lifecycle_rule {
+    id = "ece592-flowlogs-rule"
+    enabled = true
+
+		transition {
+      days          = 180
+      storage_class = "GLACIER"
+    } 
+  }
+
+  tags = {
+    Name = "ece592-flowlogs-brando"
+  }
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "delivery.logs.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::ece592-flowlogs-brando"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "delivery.logs.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::ece592-flowlogs-brando/*",
             "Condition": {
                 "StringEquals": {
                     "s3:x-amz-acl": "bucket-owner-full-control"
